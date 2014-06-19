693be09836eb25502c437d72f985efd6466f2ac7
Sébastien Lerique <sl@mehho.net>
2014-06-20 01:50:55 +0200

    Add nickname input and submit buttons to index

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -10,15 +10,30 @@ module('Acceptances - Index', {
 });
 
 test('index renders', function(){
-  expect(3);
+  expect(10);
 
   visit('/').then(function(){
     var title = find('h2#title');
     var list = find('ul li');
+    var pPick = find('p#pick');
+    var inputNickname = find('input[name=nickname]');
+    var buttonSeed = find('button[name=seed]');
+    var buttonInterpret = find('button[name=interpret]');
 
-    equal(title.text(), 'Welcome to Gistr');
+    equal(title.text(), 'Gistr');
 
     equal(list.length, 3);
     equal(list.text(), 'redyellowblue');
+
+    equal(pPick.text(), 'Pick a nickname');
+
+    equal(inputNickname.attr('placeholder'), 'e.g. hoory-horse');
+    equal(inputNickname.attr('autofocus'), 'autofocus');
+
+    equal(buttonSeed.text(), 'New seed');
+    equal(buttonSeed.attr('type'), 'submit');
+
+    equal(buttonInterpret.text(), 'Interpret');
+    equal(buttonInterpret.attr('type'), 'submit');
   });
 });
