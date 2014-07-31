fda3f0f2e30ec6800db5921b940852f610d590c6
Sébastien Lerique <sl@mehho.net>
2014-07-31 18:34:02 +0200

    Add a Back button in play, and test navigation

--- tests/acceptance/play-test.js
+++ tests/acceptance/play-test.js
@@ -10,15 +10,38 @@ module('Acceptances - Play', {
 });
 
 test('play renders', function() {
-  expect(3);
+  expect(5);
 
-  visit('/').then(function() {
+  visit('/play').then(function() {
     var pNetstatus = find('p#netstatus');
     var sNetstatus = pNetstatus.find('span');
+    var aHome = find('a#home');
 
     equal(pNetstatus.text(), 'Network status: ' + sNetstatus.text());
 
     equal(sNetstatus.text(), 'checking');
     equal(sNetstatus.attr('class'), 'checking');
+
+    equal(aHome.text(), 'Home');
+    equal(aHome.attr('href'), '/');
+  });
+});
+
+test('navigate from home to play and back', function() {
+  expect(6);
+
+  visit('/');
+  click('#play');
+  andThen(function() {
+    equal(currentRouteName(), 'play.read');
+    equal(currentPath(), 'play.read');
+    equal(currentURL(), '/play/read');
+  });
+
+  click('#home');
+  andThen(function() {
+    equal(currentRouteName(), 'index');
+    equal(currentPath(), 'index');
+    equal(currentURL(), '/');
   });
 });
