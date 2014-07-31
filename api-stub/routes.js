9180ab5bc4f42f2c6618198036cea7c605772ef4
Sébastien Lerique <sl@mehho.net>
2014-07-31 19:45:05 +0200

    Remove erroneous error preventing grunt from completing

--- api-stub/routes.js
+++ api-stub/routes.js
@@ -3,6 +3,6 @@ module.exports = function(server) {
   // Create an API namespace, so that the root does not
   // have to be repeated for each end point.
   server.namespace('/api/v1', function() {
-    throw new Error('Not implemented because not used.');
+    // Not implemented
   });
 };
