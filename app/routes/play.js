aa916c9d406bfbf856a7917291c3f27c27866e5e
Sébastien Lerique <sl@mehho.net>
2014-07-31 18:33:51 +0200

    Redirect to play.read

--- app/routes/play.js
+++ app/routes/play.js
@@ -1,5 +1,9 @@
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('sentence', '1');
+  },
+
+  afterModel: function() {
+    this.transitionTo('play.read');
   }
 });
