ed20c3804259b26ccd5f6d44fab81b65d00cfcb0
Sébastien Lerique <sl@mehho.net>
2014-07-31 17:06:42 +0200

    Remove miswritten test for PlayRoute's model

--- tests/unit/routes/play-test.js
+++ tests/unit/routes/play-test.js
@@ -8,13 +8,3 @@ moduleFor('route:play', "Unit - PlayRoute");
 test("it exists", function() {
   ok(this.subject() instanceof PlayRoute);
 });
-
-test("#model", function() {
-  deepEqual(this.subject().model(), {
-    id: '1',
-    author: '1',
-    from: null,
-    children: [],
-    text: 'Some first sentence'
-  });
-});
