07f9782081edb48788a71f3694a7d3aa48eb8fce
Sébastien Lerique <sl@mehho.net>
2014-07-31 14:17:45 +0200

    Code style

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -5,10 +5,10 @@ import IndexRoute from 'appkit/routes/index';
 
 moduleFor('route:index', "Unit - IndexRoute");
 
-test("it exists", function(){
+test("it exists", function() {
   ok(this.subject() instanceof IndexRoute);
 });
 
-test("#model", function(){
+test("#model", function() {
   deepEqual(this.subject().model(), ['red', 'yellow', 'blue']);
 });
