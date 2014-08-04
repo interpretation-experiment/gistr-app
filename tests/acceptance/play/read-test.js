b442ecbcd546ba010d4e271c75a3df2f1653ee98
Sébastien Lerique <sl@mehho.net>
2014-08-04 15:26:42 +0200

    Add a test play/read page, with tests

---
+++ tests/acceptance/play/read-test.js
@@ -0,0 +1,54 @@
+var App;
+
+module('Acceptances - Play/Read', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('play/read renders', function() {
+  expect(7);
+
+  visit('/play/read');
+  andThen(function() {
+    var pNetstatus = find('p#netstatus');
+    var sNetstatus = pNetstatus.find('span');
+    var aHome = find('a#home');
+    var pInstructions = find('p#instructions');
+    var bqText = find('blockquote#text');
+
+    equal(pNetstatus.text(), 'Network status: ' + sNetstatus.text());
+
+    equal(sNetstatus.text(), 'checking');
+    equal(sNetstatus.attr('class'), 'checking');
+
+    equal(aHome.text(), 'Home');
+    equal(aHome.attr('href'), '/');
+
+    equal(pInstructions.text(), 'Read this carefully:');
+
+    equal(bqText.text(), 'Some first sentence');
+  });
+});
+
+test('navigate from home to play and back', function() {
+  expect(6);
+
+  visit('/');
+  click('#play');
+  andThen(function() {
+    equal(currentRouteName(), 'play.read');
+    equal(currentPath(), 'play.read');
+    equal(currentURL(), '/play/read');
+  });
+
+  click('#home');
+  andThen(function() {
+    equal(currentRouteName(), 'index');
+    equal(currentPath(), 'index');
+    equal(currentURL(), '/');
+  });
+});
