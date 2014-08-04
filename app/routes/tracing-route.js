0f409d15ccf1dd4143a20f84842ae3dab8fb4cd4
Sébastien Lerique <sl@mehho.net>
2014-08-04 18:50:00 +0200

    Add some debug logging

--- app/routes/tracing-route.js
+++ app/routes/tracing-route.js
@@ -1,4 +1,9 @@
 export default Ember.Route.extend({
+  beforeModel: function(transition) {
+    Ember.Logger.log("Transition #" + transition.sequence + ": to TracingRoute: " +
+                     transition.originName + " -> " + transition.targetName);
+  },
+
   actions: {
     willTransition: function(transition){
       this._willTransition(transition);
@@ -6,6 +11,8 @@ export default Ember.Route.extend({
   },
 
   _willTransition: function(transition) {
-    transition.originName = this.routeName;
+    transition.originName = this.get('routeName');
+    Ember.Logger.log("Transition #" + transition.sequence + ": from TracingRoute: " +
+                     transition.originName + " -> " + transition.targetName);
   }
 });
