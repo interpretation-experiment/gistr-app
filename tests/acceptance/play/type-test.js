d54baf7101b05a41b0feccea72141fa519babef6
Sébastien Lerique <sl@mehho.net>
2014-08-04 16:28:29 +0200

    Add a play.type page with tests

---
+++ tests/acceptance/play/type-test.js
@@ -0,0 +1,61 @@
+var App;
+
+module('Acceptances - Play/Type', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('play/type renders', function() {
+  expect(11);
+
+  visit('/play/type');
+  andThen(function() {
+    var pNetstatus = find('p#netstatus');
+    var sNetstatus = pNetstatus.find('span');
+    var aHome = find('a#home');
+    var pInstructions = find('p#instructions');
+    var fForm = find('form');
+    var taText = find('textarea[name=text]');
+    var bSend = find('button[name=send]');
+
+    equal(pNetstatus.text(), 'Network status: ' + sNetstatus.text());
+
+    equal(sNetstatus.text(), 'checking');
+    equal(sNetstatus.attr('class'), 'checking');
+
+    equal(aHome.text(), 'Home');
+    equal(aHome.attr('href'), '/');
+
+    equal(pInstructions.text(), 'Type the sentence as you remember it:');
+
+    equal(fForm.attr('action'), '#');
+    equal(fForm.attr('method'), 'post');
+
+    equal(taText.attr('autofocus'), 'autofocus');
+
+    equal(bSend.text(), 'Send');
+    equal(bSend.attr('type'), 'submit');
+  });
+});
+
+test('navigate to home', function() {
+  expect(6);
+
+  visit('/play/type');
+  andThen(function() {
+    equal(currentRouteName(), 'play.type');
+    equal(currentPath(), 'play.type');
+    equal(currentURL(), '/play/type');
+  });
+
+  click('#home');
+  andThen(function() {
+    equal(currentRouteName(), 'index');
+    equal(currentPath(), 'index');
+    equal(currentURL(), '/');
+  });
+});
