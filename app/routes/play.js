2ec1f18759ba73acebc1e321a38771fdea119cfa
Sébastien Lerique <sl@mehho.net>
2014-08-08 13:50:02 +0200

    Code style

--- app/routes/play.js
+++ app/routes/play.js
@@ -1,7 +1,7 @@
 export default Ember.Route.extend({
   // FIXME: untested
   model: function() {
-  // FIXME: make sure it's reset at each visit
+    // FIXME: make sure it's reset at each visit
     return this.store.find('sentence', '1');
   }
 });
