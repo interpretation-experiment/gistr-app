2d8092257e31b707794c0d8ad8e36e403c8cb253
Sébastien Lerique <sl@mehho.net>
2014-08-26 15:40:12 +0200

    Add tests on timer durations, change some code style

--- tests/unit/controllers/play/read-test.js
+++ tests/unit/controllers/play/read-test.js
@@ -33,12 +33,24 @@ test("cancelCountdown calls the right method", function() {
   controller.send('cancelCountdown');
 });
 
-test("_startCountdown starts transitionTimer, sets countdown, and reschedules",
-     function() {
-  expect(5);
+test("_startCountdown starts transitionTimer with proper delay, " +
+     "sets countdown, and reschedules", function() {
+  expect(6);
 
   var controller = this.subject(),
       context = { goodContext: true };
+
+  // Shorten transitionTimer to speed up test
+  controller.duration = 0.25;
+
+  controller._setCountdown = function(value) {
+    equal(value, 0.25);
+  };
+  controller._reschedule = function() {
+    ok(true);
+  };
+  controller.set('content', {});
+  var now = Date.now();
 
   return new Ember.RSVP.Promise(function(resolve, reject) {
     var callback = function() {
@@ -46,19 +58,11 @@ test("_startCountdown starts transitionTimer, sets countdown, and reschedules",
       ok(true);
       // called in the right context
       ok(this.goodContext);
+      // less than 5ms difference, allowing for runtime imprecisions
+      ok(Math.abs(Date.now() - now - 250) < 5);
       // resolve promise to close test
       resolve(true);
     };
-
-    // Shorten transitionTimer
-    controller.duration = 1;
-    controller._setCountdown = function(value) {
-      equal(value, 1);
-    };
-    controller._reschedule = function() {
-      ok(true);
-    };
-    controller.set('content', {});
 
     controller._startCountdown(context, callback);
     ok(!!controller.get('transitionTimer'));
@@ -66,7 +70,7 @@ test("_startCountdown starts transitionTimer, sets countdown, and reschedules",
 });
 
 test("_updateCountdown sets correct new countdown and reschedules", function() {
-  expect(3);
+  expect(2);
 
   var controller = this.subject();
 
@@ -74,8 +78,8 @@ test("_updateCountdown sets correct new countdown and reschedules", function() {
   controller.set('lastNow', Date.now() - 50);  // now - 50ms
   controller.set('countdownPrec', 1);
   controller._setCountdown = function(value) {
-    ok(value > 0.940);
-    ok(value < 0.960);
+    // less than 5ms difference, allowing for runtime imprecisions
+    ok(Math.abs(value - 0.95) < 0.005);
   };
   controller._reschedule = function() {
     ok(true);
@@ -89,14 +93,14 @@ test("_cancelCountdown cancels transitionTimer", function() {
   var controller = this.subject(),
       transitionTimerHasRun = false;
 
-  return new Ember.RSVP.Promise(function(resolve, reject) {
-    controller.set('content', {});
-    // Set a transitionTimer that fails the test
-    var transitionTimer = Ember.run.later(this, function() {
-      transitionTimerHasRun = true;
-    }, 10);
-    controller.set('transitionTimer', transitionTimer);
+  controller.set('content', {});
+  // Set a transitionTimer that fails the test
+  var transitionTimer = Ember.run.later(this, function() {
+    transitionTimerHasRun = true;
+  }, 10);
+  controller.set('transitionTimer', transitionTimer);
 
+  return new Ember.RSVP.Promise(function(resolve, reject) {
     // Set a timer to check transitionTimer was cancelled
     Ember.run.later(this, function() {
       if (transitionTimerHasRun) {
@@ -119,14 +123,14 @@ test("_cancelCountdown cancels renderTimer", function() {
   var controller = this.subject(),
       renderTimerHasRun = false;
 
-  return new Ember.RSVP.Promise(function(resolve, reject) {
-    controller.set('content', {});
-    // Set a renderTimer that fails the test
-    var renderTimer = Ember.run.later(this, function() {
-      renderTimerHasRun = true;
-    }, 10);
-    controller.set('renderTimer', renderTimer);
+  controller.set('content', {});
+  // Set a renderTimer that fails the test
+  var renderTimer = Ember.run.later(this, function() {
+    renderTimerHasRun = true;
+  }, 10);
+  controller.set('renderTimer', renderTimer);
 
+  return new Ember.RSVP.Promise(function(resolve, reject) {
     // Set a timer to check renderTimer was cancelled
     Ember.run.later(this, function() {
       if (renderTimerHasRun) {
@@ -143,7 +147,7 @@ test("_cancelCountdown cancels renderTimer", function() {
   });
 });
 
-test("_setCountdown sets new countdown", function() {
+test("_setCountdown sets new countdown value", function() {
   expect(2);
 
   var controller = this.subject();
