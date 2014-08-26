c778f0befc3b6522112315d265db2d54b27638df
Sébastien Lerique <sl@mehho.net>
2014-08-26 13:39:10 +0200

    Add controller:play test

--- tests/unit/controllers/play/read-test.js
+++ tests/unit/controllers/play/read-test.js
@@ -9,3 +9,15 @@ test("it exists", function() {
   expect(1);
   ok(this.subject() instanceof PlayReadController);
 });
+
+test("startCountdown calls the right method", function() {
+  expect(2);
+
+  var controller = this.subject();
+  controller._startCountdown = function(route, callback) {
+    equal(route, 'dummy-route');
+    equal(callback, 'dummy-callback');
+  };
+
+  controller.send('startCountdown', 'dummy-route', 'dummy-callback');
+});
