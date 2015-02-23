88afe804a27015cb62b15e35e409c0f5cacf9315
Sébastien Lerique <sl@mehho.net>
2015-02-23 18:15:48 -0300

    Inform the user of his current profile status

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -3,4 +3,15 @@ import Ember from 'ember';
 import SessionMixin from 'gistr/mixins/session';
 
 
-export default Ember.Controller.extend(SessionMixin);
+export default Ember.Controller.extend(SessionMixin, {
+  isProfileIncomplete: function() {
+    return this.get('profileErrors.length') > 0;
+  }.property('profileErrors.length'),
+  profileErrors: function() {
+    if (!this.get('currentProfile')) {
+      return ['Set your mothertongue'];
+    } else {
+      return [];
+    }
+  }.property('currentProfile')
+});
