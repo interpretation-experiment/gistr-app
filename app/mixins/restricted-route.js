313a24bc373aa0dc93612a22afd55dc58746af91
Sébastien Lerique <sl@mehho.net>
2015-03-17 00:29:30 -0300

    Use PNotify for notifications

--- app/mixins/restricted-route.js
+++ app/mixins/restricted-route.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 
 export default Ember.Mixin.create({
+  growl: Ember.inject.service(),
+
   beforeModel: function(transition) {
     if (!this.get('session.isAuthenticated')) {
       this.controllerFor('login').set('attemptedTransition', transition);
+      this.get('growl').notice('Oops!', 'You must sign in first!');
       this.transitionTo('login');
       return false;
     } else {
---
+++ app/services/growl.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+
+export default Ember.Service.extend({
+  _growl: function(title, text, type) {
+    new PNotify({
+      title: title,
+      text: text,
+      opacity: .8,
+      delay: 5000,
+      type: type,
+      buttons: { sticker: false },
+      animate_speed: 'fast'
+    });
+  },
+  notice: function(title, text, type) {
+    this._growl(title, text, "notice");
+  },
+  info: function(title, text, type) {
+    this._growl(title, text, "info");
+  },
+  success: function(title, text, type) {
+    this._growl(title, text, "success");
+  },
+  error: function(title, text, type) {
+    this._growl(title, text, "error");
+  }
+});
