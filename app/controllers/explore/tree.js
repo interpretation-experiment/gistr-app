a4f7b04d0944d6197c3b56cc2a74e5691ed7fe0e
Sébastien Lerique <sl@mehho.net>
2015-03-28 22:04:58 +0100

    Allow for expansion of path in tree detail view

--- app/controllers/explore/tree.js
+++ app/controllers/explore/tree.js
@@ -4,18 +4,47 @@ import SessionMixin from 'gistr/mixins/session';
 
 
 export default Ember.Controller.extend(SessionMixin, {
+
+  /*
+   * Selection display and control
+   */
   hovered: null,
-  hasSelection: Ember.computed.or('selectionStart', 'selectionEnd'),
   selectionStart: null,
   selectionEnd: null,
+  selectionPath: null,
+  showPath: false,
+  reset: function() {
+    this.setProperties({
+      hovered: null,
+      selectionStart: null,
+      selectionEnd: null,
+      selectionPath: null,
+      showPath: false
+    });
+  },
+
+  hasSelection: Ember.computed.or('selectionStart', 'selectionEnd'),
+  hasPath: Ember.computed.gt('selectionPath.length', 0),
 
   actions: {
     hoverSentence: function(sentence) {
       this.set("hovered", sentence);
     },
     setSelection: function(selection) {
+      this.set('showPath', false);
       this.set('selectionStart', selection.start);
       this.set('selectionEnd', selection.end);
+      if (!Ember.isNone(selection.path) && selection.path.length > 2) {
+        this.set('selectionPath', selection.path.slice(1, -1));
+      } else {
+        this.set('selectionPath', null);
+      }
+    },
+    togglePath: function() {
+      this.set('showPath', !this.get('showPath'));
+    },
+    reset: function() {
+      this.reset();
     }
   }
 });
--- app/routes/explore/tree.js
+++ app/routes/explore/tree.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 import SessionMixin from 'gistr/mixins/session';
 import ProfileRouteMixin from 'gistr/mixins/profile-route';
+import FormRouteMixin from 'gistr/mixins/form-route';
 
 
-export default Ember.Route.extend(SessionMixin, ProfileRouteMixin, {
+export default Ember.Route.extend(SessionMixin, ProfileRouteMixin, FormRouteMixin, {
   model: function(params) {
     return this.store.find('tree', params.tree_id);
   }
