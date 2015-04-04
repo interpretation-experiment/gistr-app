033b7b4cbbae9b8db0e758514eb77ea6c064ba45
Sébastien Lerique <sl@mehho.net>
2015-04-04 20:04:33 +0200

    Fix staff access to trees

--- app/abilities/tree.js
+++ app/abilities/tree.js
@@ -6,7 +6,7 @@ import SessionMixin from 'gistr/mixins/session';
 export default Ability.extend(SessionMixin, {
   canRead: function() {
     // Staff can read everything
-    if (this.get('currentProfile.isStaff')) { return true; }
+    if (this.get('currentUser.isStaff')) { return true; }
 
     var tree = this.get('model'),
         profileTrees = this.get('currentProfile.trees');
