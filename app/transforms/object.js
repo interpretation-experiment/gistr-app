5bf6fdcb3fda7d954eb3e36c61c219b215c272b8
Sébastien Lerique <sl@mehho.net>
2015-05-08 18:36:09 +0200

    Fix typo bug

--- app/transforms/object.js
+++ app/transforms/object.js
@@ -4,7 +4,7 @@ import DS from 'ember-data';
 
 export default DS.Transform.extend({
   serialize: function(deserialized) {
-    return Ember.none(deserialized) ? {} : deserialized;
+    return Ember.isNone(deserialized) ? {} : deserialized;
   },
 
   deserialize: function(serialized) {
