02a30641fbea210ef7e18443c1aaa75f6c6acc11
Sébastien Lerique <sl@mehho.net>
2015-05-09 18:26:09 +0200

    Move focused views to components

--- app/mixins/focused-element.js
+++ app/mixins/focused-element.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   focusElement: function() {
-    this.get('element').focus();
+    this.$().focus();
   }.on('didInsertElement')
 });
