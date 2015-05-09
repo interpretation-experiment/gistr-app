f76f8d4ed874bfb837cde755fcc967e37c316d70
Sébastien Lerique <sl@mehho.net>
2015-05-09 16:04:45 +0200

    Check for known infos

--- app/mixins/info.js
+++ app/mixins/info.js
@@ -6,10 +6,19 @@ export default Ember.Mixin.create({
   knownInfos: Ember.required(),
   pushInfo: function(info) {
     console.log('[push info] ' + info);
-    var infos = this.get('infos');
+
+    var infos = this.get('infos'),
+        knownInfos = this.get('knownInfos');
+
+    if (knownInfos.indexOf(infos) === -1) {
+      console.log('[push info] ' + 'unknown info, aborting');
+      return;
+    }
+
     if (infos.indexOf(info) === -1) {
       this.get('infos').push(info);
     }
+
     console.log('[infos=] ' + this.get('infos'));
   },
   resetInfos: function() {
