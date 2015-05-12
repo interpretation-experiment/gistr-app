a2ae1eff7607695b22cb3ba451fb551d63404e5a
Sébastien Lerique <sl@mehho.net>
2015-05-12 18:00:51 +0200

    Proper link if logged in

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
+import SessionMixin from 'gistr/mixins/session';
 
-export default Ember.Controller.extend({
+
+export default Ember.Controller.extend(SessionMixin, {
   lifecycle: Ember.inject.service()
 });
