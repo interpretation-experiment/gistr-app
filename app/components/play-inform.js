5e15fa6bb2e92fcbd0d9561b7f83fbf6db85c29e
Sébastien Lerique <sl@mehho.net>
2015-05-12 11:44:13 +0200

    Start fleshing out play-inform

--- app/components/play-inform.js
+++ app/components/play-inform.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
+import SessionMixin from 'gistr/mixins/session';
 import splitEvent from 'gistr/utils/split-event';
 
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(SessionMixin, {
+  growl: Ember.inject.service(),
+  lifecycle: Ember.inject.service(),
+
   events: null,
+  hasEvents: Ember.computed.notEmpty('events'),
 
   filterEvents: function(params) {
     var events = this.get('events');
@@ -21,24 +26,45 @@ export default Ember.Component.extend({
     });
   },
 
-  lifecycleEvents: function() {
-    return this.filterEvents({ type: 'lifecycle' });
+  lifecycleEvent: function() {
+    var events = this.filterEvents({ type: 'lifecycle' });
+    if (events.length > 1) {
+      throw new Error("Got more than one lifecycle event: " + events);
+    }
+    return events.objectAt(0);
   }.property(),
 
   rhythmEvents: function() {
     return this.filterEvents({ type: 'rhythm' });
   }.property(),
+  hasRhythmEvents: Ember.computed.notEmpty('rhythmEvents'),
 
   gainEvents: function() {
     return this.filterEvents({ type: 'gain' });
   }.property(),
+  growlGains: function() {
+    for (var event of this.get('gainEvents')) {
+      // No other events than 'new-credit' for now
+      if (splitEvent(event).name === 'new-credit') {
+        this.get('growl').success(
+          'New credit!',
+          'You can suggest a new sentence by going to Home > New'
+        );
+      }
+    }
+  }.on('didInsertElement'),
 
-  // EVENTS
-  //    lifecycle w/ or w/o transition [always]
-  //    gain [always]
-  //    rhythm [if no lifecycle]
-  // STATE modulates event if there are events, alone otherwise
-  //    no work left == lifecycle w/o transition
-  //    empty sentences [quiet if no work left]
-
+  emptySentences: Ember.computed.equal('currentProfile.availableTreesBucket', 0),
+  stateValidation: function() {
+    return this.get('lifecycle').validateState();
+  }.property(),
+  hasTransitioned: function() {
+    var lifecycleEvent = this.get('lifecycleEvent');
+    // Note that if this is false, then hasWorkLeft will be false
+    // i.e. with event but not transitioned => no work left
+    return this.get('lifecycle.currentState') !== splitEvent(lifecycleEvent).state;
+  }.property('lifecycle.currentState', 'lifecycleEvent'),
+  hasWorkLeft: function() {
+    return this.get('stateValidation').actionRoutes.indexOf('play') !== -1;
+  }.property('stateValidation'),
 });
