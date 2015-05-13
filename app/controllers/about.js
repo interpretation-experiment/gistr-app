4d157d5fb105dbce646def734f398a5772375a9c
Sébastien Lerique <sl@mehho.net>
2015-05-13 11:36:19 +0200

    Use actions to simplify about template

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -3,4 +3,14 @@ import Ember from 'ember';
 import SessionMixin from 'gistr/mixins/session';
 
 
-export default Ember.Controller.extend(SessionMixin);
+export default Ember.Controller.extend(SessionMixin, {
+  actions: {
+    passExperiment: function() {
+      if (this.get('session.isAuthenticated')) {
+        this.transitionToRoute('play');
+      } else {
+        this.transitionToRoute('register');
+      }
+    }
+  }
+});
