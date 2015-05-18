6b9422da859b24c73450887c190bdafd33ba6b26
Sébastien Lerique <sl@mehho.net>
2015-05-18 14:16:19 +0200

    Namespace enter-next keyup event

--- app/mixins/enter-next.js
+++ app/mixins/enter-next.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 
 export default Ember.Mixin.create({
-  keyupEvent: 'keyup',
+  keyupEvent: 'keyup.enter-next',
   initKeyListener: function() {
     var self = this;
     Ember.$(window).on(this.get('keyupEvent'), function(event) {
