9627ff08b1bc31df47e87d462db568347d90893a
Sébastien Lerique <sl@mehho.net>
2015-05-23 16:02:11 +0200

    Add unused properties of emailaddress

--- app/models/emailaddress.js
+++ app/models/emailaddress.js
@@ -5,5 +5,11 @@ export default DS.Model.extend({
   user: DS.belongsTo('user'),
   email: DS.attr('string'),
   verified: DS.attr('boolean'),
-  primary: DS.attr('boolean')
+  primary: DS.attr('boolean'),
+
+  /*
+   * Unused properties
+   */
+  url: DS.attr('string'),
+  userUrl: DS.attr('string'),
 });
