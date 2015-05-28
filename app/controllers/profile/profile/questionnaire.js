d692ae2abfac800c9917c4283d5329fa8cd066c5
Sébastien Lerique <sl@mehho.net>
2015-05-28 15:38:59 +0200

    Start questionnaire form and stub controller

--- app/controllers/profile/profile/questionnaire.js
+++ app/controllers/profile/profile/questionnaire.js
@@ -2,9 +2,20 @@ import Ember from 'ember';
 
 
 export default Ember.Controller.extend({
+  questionnaireChoices: Ember.inject.service(),
+
+  showNaiveDetail: false,
+
   actions: {
     reset: function() {
       // Nothing for now
+    },
+    toggleNaiveDetail: function() {
+      this.set('naiveDetail', '');
+      this.toggleProperty('showNaiveDetail');
+      if (this.get('showNaiveDetail')) {
+        this.set('notNaive', true);
+      }
     },
   }
 });
