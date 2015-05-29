76a726f1217b54248983fcb264bc76c393ccb8c4
Sébastien Lerique <sl@mehho.net>
2015-05-29 17:12:45 +0200

    Restrict questionnaire if no profile yet

--- app/routes/profile/profile/questionnaire.js
+++ app/routes/profile/profile/questionnaire.js
@@ -2,12 +2,15 @@ import Ember from 'ember';
 
 import FormRouteMixin from 'gistr/mixins/form-route';
 import SessionMixin from 'gistr/mixins/session';
+import ProfileRouteMixin from 'gistr/mixins/profile-route';
 
 
-export default Ember.Route.extend(FormRouteMixin, SessionMixin, {
-  beforeModel: function() {
-    if (this.get('currentProfile.questionnaireDone')) {
-      this.transitionTo('profile.profile');
+export default Ember.Route.extend(FormRouteMixin, SessionMixin, ProfileRouteMixin, {
+  beforeModel: function(transition) {
+    if (this._super(transition)) {
+      if (this.get('currentProfile.questionnaireDone')) {
+        this.transitionTo('profile.profile');
+      }
     }
   },
   scrollUp: function() {
