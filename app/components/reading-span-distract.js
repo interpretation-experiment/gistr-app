726a2aa1ec154865f596781736a68f475aaffd85
Sébastien Lerique <sl@mehho.net>
2015-05-31 17:44:58 +0200

    Do reading-span-distract

--- app/components/reading-span-distract.js
+++ app/components/reading-span-distract.js
@@ -1,10 +1,30 @@
 import Ember from 'ember';
 
+import TimefulMixin from 'gistr/mixins/timeful';
+import EnterNextMixin from 'gistr/mixins/enter-next';
 
-export default Ember.Component.extend({
+
+export default Ember.Component.extend(TimefulMixin, EnterNextMixin, {
+  duration: 2,  // seconds
+  timeLeft: function() {
+    var duration = this.get('duration'),
+        pProgress = this.get('progress') / 100;
+    return 1 + Math.floor(duration * (1 -pProgress));
+  }.property('progress'),
+  wait: true,
+  colors: ['blue', 'blue', 'green'],
+
+  timerDone: function() {
+    this.set('wait', false);
+  },
+  onEnter: function() {
+    this.send('next');
+  },
   actions: {
     next: function() {
-      this.sendAction('next');
+      if (!this.get('wait')) {
+        this.sendAction('next');
+      }
     }
   }
 });
