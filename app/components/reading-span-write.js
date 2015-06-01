eed7b33fec4251ecb6caafd4302636f5009c9461
Sébastien Lerique <sl@mehho.net>
2015-06-01 10:14:32 +0200

    Prevent pasting

--- app/components/reading-span-write.js
+++ app/components/reading-span-write.js
@@ -2,6 +2,21 @@ import Ember from 'ember';
 
 
 export default Ember.Component.extend({
+  /*
+   * Copy-paste prevention
+   */
+  pasteEvent: 'paste.reading-span-write',
+  initPastePrevention: function() {
+    var growl = this.get('growl');
+    Ember.$(window).on(this.get('pasteEvent'), function(event) {
+      event.preventDefault();
+      growl.error("No copy-pasting", "Don't copy-paste the text, it won't work!");
+    });
+  }.on('didInsertElement'),
+  closePastePrevention: function() {
+    Ember.$(window).off(this.get('pasteEvent'));
+  }.on('willDestroyElement'),
+
   actions: {
     save: function() {
       this.sendAction('save');
