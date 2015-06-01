6997a965860810fcd9d7e5885b9a0fd25986a772
Sébastien Lerique <sl@mehho.net>
2015-06-01 12:34:04 +0200

    Remove logging

--- app/helpers/h-capitalize.js
+++ app/helpers/h-capitalize.js
@@ -3,6 +3,5 @@ import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function(value/*, options*/) {
   if (Ember.isNone(value)) { return; }
-  console.log(value);
   return value.capitalize();
 });
