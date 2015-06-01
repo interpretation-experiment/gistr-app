7fc7539004dcd030610f65846b7289c3cb2f7966
Sébastien Lerique <sl@mehho.net>
2015-06-01 10:13:51 +0200

    Prevent copying

--- app/components/reading-span-read.js
+++ app/components/reading-span-read.js
@@ -7,13 +7,31 @@ import EnterNextMixin from 'gistr/mixins/enter-next';
 export default Ember.Component.extend(TimefulMixin, EnterNextMixin, {
   shaping: Ember.inject.service(),
 
+  /*
+   * Copy-paste prevention
+   */
+  copyEvent: 'copy.reading-span-read',
+  initCopyPrevention: function() {
+    var growl = this.get('growl');
+    Ember.$(window).on(this.get('copyEvent'), function(event) {
+      event.preventDefault();
+      growl.error("No copy-pasting", "Don't copy-paste the text, it won't work!");
+    });
+  }.on('didInsertElement'),
+  closeCopyPrevention: function() {
+    Ember.$(window).off(this.get('copyEvent'));
+  }.on('willDestroyElement'),
+
+  /*
+   * Timing
+   */
   duration: function() {
     return this.get('shaping.readFactor') * this.get('shaping.readingSpanWordsCount');
   }.property('shaping.readFactor', 'sentenceTokensCount'),
-
   timerDone: function() {
     this.sendAction('next');
   },
+
   onEnter: function() {
     this.send('next');
   },
