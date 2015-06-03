17fb83e77acd1feceacdbfa497c421694ac73e8f
Sébastien Lerique <sl@mehho.net>
2015-06-03 15:21:18 +0200

    Add pagination to trees

--- app/controllers/explore/index.js
+++ app/controllers/explore/index.js
@@ -3,4 +3,17 @@ import Ember from 'ember';
 import SessionMixin from 'gistr/mixins/session';
 
 
-export default Ember.Controller.extend(SessionMixin);
+export default Ember.Controller.extend(SessionMixin, {
+  // Pagination query parameters
+  queryParams: ["page", "perPage"],
+
+  // Pagination bindings
+  pageBinding: "content.page",
+  perPageBinding: "content.perPage",
+  totalPagesBinding: "content.totalPages",
+
+  // Pagination state variables
+  page: 1,
+  perPage: 10,
+  perPageOptions: [5, 10, 25, 50, 100],
+});
--- app/routes/explore/index.js
+++ app/routes/explore/index.js
@@ -1,14 +1,29 @@
 import Ember from 'ember';
 
+import PaginationRouteMixin from 'ember-cli-pagination/remote/route-mixin';
+
 import SessionMixin from 'gistr/mixins/session';
+import ceiling from 'gistr/utils/ceiling';
 
 
-export default Ember.Route.extend(SessionMixin, {
-  model: function() {
-    if (this.get('currentUser.isStaff')) {
-      return this.store.find('tree', { page_size: 100 });
-    } else {
-      return this.get('currentProfile.trees');
+export default Ember.Route.extend(SessionMixin, PaginationRouteMixin, {
+  model: function(params) {
+    params.paramMapping = {
+      page: 'page',
+      perPage: 'page_size',
+      total_pages: [
+        'count',
+        function({rawVal, page, perPage}) {
+          if (Ember.isNone(perPage) || perPage === 0) { return rawVal; }
+          return ceiling(rawVal / perPage);
+        }
+      ]
+    };
+    // Restrict to own trees if not staff
+    var profile = this.get('currentProfile');
+    if (!profile.get('isStaff')) {
+      params.profile = profile.get('id');
     }
+    return this.findPaged('tree', params);
   }
 });
