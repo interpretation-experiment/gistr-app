cd6d83e426525469bb9b71a913d2a2ce2a083656
Sébastien Lerique <sl@mehho.net>
2015-06-08 10:41:48 +0200

    Add a profile-chart component, used with dummy values for the moment

--- app/controllers/reports.js
+++ app/controllers/reports.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
+var d3 = window.d3;
+
 
 export default Ember.Controller.extend({
+  randomValues: d3.range(1000).map(d3.random.normal()),
   actions: {
     passExperiment: function() {
       if (this.get('session.isAuthenticated')) {
