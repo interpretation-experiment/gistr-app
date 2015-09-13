4ffc50e931062b8b5f5db65a14a99f12ca243ee1
Sébastien Lerique <sl@mehho.net>
2015-09-13 19:14:08 +0200

    Wrap async calls in Ember.run

--- tests/unit/controllers/profile/profile/word-span-test.js
+++ tests/unit/controllers/profile/profile/word-span-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:profile/profile/word-span', {
@@ -17,26 +18,32 @@ test('span is properly computed', function(assert) {
   var controller = this.subject();
 
   // The last size to have the minimum acceptable level (2/3) is the span
-  controller.set('spanStats', {
-    3: [true, true, true],
-    4: [true, false, false],
-    5: [true, false, false],
+  Ember.run(function() {
+    controller.set('spanStats', {
+      3: [true, true, true],
+      4: [true, false, false],
+      5: [true, false, false],
+    });
   });
   assert.equal(controller.computeSpan(), 3);
   assert.equal(controller.get('span'), 3);
-  controller.set('spanStats', {
-    3: [true, true, true],
-    4: [true, true, false],
-    5: [true, false, false],
+  Ember.run(function() {
+    controller.set('spanStats', {
+      3: [true, true, true],
+      4: [true, true, false],
+      5: [true, false, false],
+    });
   });
   assert.equal(controller.computeSpan(), 4);
   assert.equal(controller.get('span'), 4);
-  controller.set('spanStats', {
-    3: [true, true, true],
-    4: [true, true, true],
-    5: [true, true, false],
-    6: [true, false, true],
-    7: [true, false, false],
+  Ember.run(function() {
+    controller.set('spanStats', {
+      3: [true, true, true],
+      4: [true, true, true],
+      5: [true, true, false],
+      6: [true, false, true],
+      7: [true, false, false],
+    });
   });
   assert.equal(controller.computeSpan(), 6);
   assert.equal(controller.get('span'), 6);
