8e740a3426edc7fdd4fe90dcbe1a250e64b01750
Sébastien Lerique <sl@mehho.net>
2016-11-24 15:51:02 +0100

    feat/webpack: use env function

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,48 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var extractVendorCss = new ExtractTextPlugin({ filename: 'vendor.css', allChunks: true });
 
-module.exports = {
-  entry: {
-    app: [
-      './src/index.js'
-    ]
-  },
+module.exports = function(env) {
+  return {
+    entry: {
+      app: [
+        './src/index.js'
+      ]
+    },
 
-  output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js',
-  },
+    output: {
+      path: path.resolve(__dirname + '/dist'),
+      filename: '[name].js',
+    },
 
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: extractVendorCss.extract('css-loader'),
-      },
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'file-loader?name=[name].[ext]',
-      },
-      {
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loaders: ['elm-hot-loader', 'elm-webpack-loader?verbose=true&warn=true&debug=true'],
-      },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: extractVendorCss.extract('css-loader'),
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          loader: 'file-loader?name=[name].[ext]',
+        },
+        {
+          test: /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          loaders: ['elm-hot-loader', 'elm-webpack-loader?verbose=true&warn=true&debug=true'],
+        },
+      ],
+
+      noParse: /\.elm$/,
+    },
+
+    plugins: [
+      extractVendorCss
     ],
 
-    noParse: /\.elm$/,
-  },
-
-  plugins: [
-    extractVendorCss
-  ],
-
-  devServer: {
-    inline: true,
-    historyApiFallback: true,
-    stats: 'errors-only',
-  },
-
+    devServer: {
+      inline: true,
+      historyApiFallback: true,
+      stats: 'errors-only',
+    },
+  }
 };
