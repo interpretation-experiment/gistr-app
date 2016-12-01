interpretation-experiment/gistr-app

commit 29f2f69bab6d6532217ccdbae178ccb116d342c2

feat/navigation: upgrade to new navigation module
For now I'm going to stick with my custom UrlQueryParser (although the
new UrlParser implements it in another flavour) until the usage of
UrlParser clears up in the community.

src/Helpers.elm
@@ -39,7 +39,6 @@ import Http
 import Json.Decode as JD
 import List
 import List.Extra exposing (splitAt)
-import List.Nonempty as Nonempty
 import Model exposing (Model)
 import Msg exposing (Msg(NavigateTo, Error))
 import Random
@@ -117,19 +116,19 @@ updateProfile model profile =
 
 
 navigateTo : Model -> Router.Route -> ( Model, Cmd Msg )
-navigateTo model route =
+navigateTo model request =
     let
-        authRoute =
-            Router.normalize model.auth (Debug.log "nav request" route)
-
-        emptyOnChange =
-            if authRoute /= model.route then
-                Model.emptyForms
-            else
-                identity
+        finalRoute =
+            Router.normalize model.auth (Debug.log "nav request" request)
     in
-        emptyOnChange { model | route = (Debug.log "nav final" authRoute) }
-            ! Cmds.cmdsForRoute model authRoute
+        if finalRoute /= model.route then
+            let
+                newModel =
+                    Model.emptyForms { model | route = (Debug.log "nav final" finalRoute) }
+            in
+                newModel ! Cmds.cmdsForModel newModel
+        else
+            model ! []
 
 
 authenticatedOrIgnore :

src/Cmds.elm
@@ -1,4 +1,4 @@
-module Cmds exposing (cmdsForRoute)
+module Cmds exposing (cmdsForModel)
 
 import Api
 import Experiment.Msg as ExperimentMsg
@@ -7,14 +7,13 @@ import Model exposing (Model)
 import Msg exposing (Msg)
 import Profile.Msg as ProfileMsg
 import Router
-import Store
 import Task
 import Types
 
 
-cmdsForRoute : Model -> Router.Route -> List (Cmd Msg)
-cmdsForRoute model route =
-    case route of
+cmdsForModel : Model -> List (Cmd Msg)
+cmdsForModel model =
+    case model.route of
         Router.Profile (Router.Confirm key) ->
             authenticatedOrIgnore model <|
                 \auth ->

src/Main.elm
@@ -3,14 +3,9 @@ module Main exposing (..)
 import Animation
 import Auth.Msg as AuthMsg
 import Dict
-import Experiment.Model as ExpModel
-import Experiment.Msg as ExpMsg
 import Experiment.Subscription as ExpSub
 import Form
-import Helpers exposing ((!!))
-import Intro
 import LocalStorage
-import Maybe.Extra exposing ((?))
 import Model exposing (Model)
 import Msg exposing (Msg)
 import Navigation
@@ -20,55 +15,14 @@ import Update
 import View
 
 
-main : Program Never
+main : Program Never Model Msg
 main =
-    Navigation.program (Navigation.makeParser Router.locationParser)
+    Navigation.program (uncurry Msg.UrlUpdate << Router.parse)
         { init = init
         , view = View.view
         , update = Update.update
-        , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
-
-
-
--- URL UPDATE
-
-
-urlUpdate : ( String, Maybe Router.Route ) -> Model -> ( Model, Cmd Msg )
-urlUpdate ( newUrl, maybeRoute ) model =
-    let
-        modelUrl =
-            Router.toUrl model.route
-    in
-        if (Debug.log "url" newUrl) /= modelUrl then
-            -- URL has changed, do something about it
-            let
-                ( finalModel, navigationCmd ) =
-                    Helpers.navigateTo model (maybeRoute ? model.route)
-
-                finalRoute =
-                    finalModel.route
-
-                finalUrl =
-                    Router.toUrl finalRoute
-            in
-                if finalRoute /= model.route then
-                    -- Update the model and return corresponding commands,
-                    -- and also fix the browser's url if necessary.
-                    ( finalModel, navigationCmd )
-                        !! (if newUrl /= finalUrl then
-                                [ Navigation.modifyUrl (Debug.log "url correction" finalUrl) ]
-                            else
-                                []
-                           )
-                else
-                    -- Then necessarily newUrl /= finalUrl. So don't update the model,
-                    -- but fix the browser's url.
-                    model ! [ Navigation.modifyUrl (Debug.log "url correction" finalUrl) ]
-        else
-            -- URL hasn't changed, do nothing
-            model ! []
 
 
 
@@ -99,11 +53,15 @@ subscriptions model =
 -- INIT
 
 
-init : ( String, Maybe Router.Route ) -> ( Model, Cmd Msg )
-init ( url, maybeRoute ) =
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
     -- Don't trigger any navigation (-> url update and/or redirection) due to
     -- the initial url, just wait for the tokenGet subscription to return a
     -- GotLocalToken, and that will trigger a navigation either way. Doing
     -- otherwise creates two competing navigation events (so url updates and/or
     -- redirections): the initial one, and the one from GotLocalToken.
-    Model.initialModel (maybeRoute ? Router.Home) ! [ LocalStorage.tokenGet ]
+    let
+        ( _, route ) =
+            Router.parse location
+    in
+        Model.initialModel route ! [ LocalStorage.tokenGet ]

src/Msg.elm
@@ -16,6 +16,7 @@ type Msg
       -- NOTIFICATIONS
     | Notify (Notification.Msg String)
       -- NAVIGATION
+    | UrlUpdate String Router.Route
     | NavigateTo Router.Route
     | Error Types.Error
       -- STORE

src/Router.elm
@@ -2,7 +2,7 @@ module Router
     exposing
         ( ProfileRoute(..)
         , Route(..)
-        , locationParser
+        , parse
         , normalize
         , toUrl
         )
@@ -146,20 +146,16 @@ normalize auth route =
 -- URL -> ROUTE
 
 
-locationParser : Navigation.Location -> ( String, Maybe Route )
-locationParser location =
+parse : Navigation.Location -> ( String, Route )
+parse location =
     let
-        path =
-            location.pathname
-
-        query =
-            location.search
+        url =
+            location.pathname ++ location.search
     in
-        ( path ++ query
-        , String.dropLeft 1 path
-            ++ query
+        ( url
+        , String.dropLeft 1 url
             |> UrlQueryParser.parse identity urlParser
-            |> Result.toMaybe
+            |> Result.withDefault Home
         )
 
 

src/Update.elm
@@ -11,7 +11,6 @@ import Navigation
 import Notification
 import Profile.Update as ProfileUpdate
 import Router
-import Store
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -63,6 +62,34 @@ doUpdate msg model =
         {-
            NAVIGATION
         -}
+        UrlUpdate newUrl route ->
+            if (Debug.log "url" newUrl) /= Router.toUrl model.route then
+                -- URL has changed, do something about it
+                let
+                    ( finalModel, navigationCmd ) =
+                        Helpers.navigateTo model route
+
+                    finalUrl =
+                        Router.toUrl finalModel.route
+
+                    urlCorrection =
+                        if newUrl /= finalUrl then
+                            Navigation.modifyUrl (Debug.log "url correction" finalUrl)
+                        else
+                            Cmd.none
+                in
+                    if finalModel.route /= model.route then
+                        -- Update the model and return corresponding commands,
+                        -- and also fix the browser's url if necessary.
+                        finalModel ! [ navigationCmd, urlCorrection ]
+                    else
+                        -- Then necessarily newUrl /= finalUrl. So don't update the model,
+                        -- but fix the browser's url.
+                        model ! [ urlCorrection ]
+            else
+                -- URL hasn't changed, do nothing
+                model ! []
+
         NavigateTo route ->
             let
                 ( newModel, cmd ) =

commit 0885fdff11ad33f125953a7eb7d1e86af3156693

fix/helpers: always trigger commands for a route navigated to
This fixes a bug introduced in 29f2f69bab6d6532217ccdbae178ccb116d342c2,
where we wouldn't trigger the commands if the route hadn't changed. The
problem is that when the login cycle finishes, most of the time the
route doesn't change (since we were waiting for the cycle to finish) and
we need to run commands (e.g. confirm email, get the word span in the
profile dashboard, etc.).

src/Helpers.elm
@@ -130,15 +130,14 @@ navigateTo model request =
     let
         finalRoute =
             Router.normalize model.auth (Debug.log "nav request" request)
+
+        newModel =
+            if finalRoute /= model.route then
+                Model.emptyForms { model | route = (Debug.log "nav final" finalRoute) }
+            else
+                model
     in
-        if finalRoute /= model.route then
-            let
-                newModel =
-                    Model.emptyForms { model | route = (Debug.log "nav final" finalRoute) }
-            in
-                newModel ! Cmds.cmdsForModel newModel
-        else
-            model ! []
+        newModel ! Cmds.cmdsForModel newModel
 
 
 authenticatedOrIgnore :